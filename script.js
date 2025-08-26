238c954c331caa06adcd7d3b720c08c009255e0c
mahalakshmi-1615 <mahalakshmi.marimuthu1612@gmail.com>
2025-08-27 00:26:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".sidebar .nav-link");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+navLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
